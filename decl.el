91f4a2337917846ec73ce12ad79fbef67f0bfec1
Preetpal Sohal <preetpal_sohal@hotmail.com>
2014-02-17 18:10:21 -0800

    Added sample demonstrating usage of library in Emacs init file

--- decl.el
+++ decl.el
@@ -15,6 +15,45 @@
 (require 'dash)
 ;;;; END OF DEPENDENCIES
 
+;; ;;; init.el --- sample init.el using library
+;;
+;; (package-initialize)
+;; (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+;; (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+;;
+;; (add-hook 
+;;  'after-init-hook
+;;  (lambda () "Your init file"
+;;    (require 'decl)
+;;    (decl-block :init)
+;;    (decl-node :mac-osx :init (lambda () (eq system-type 'darwin)))
+;;    (decl-node :windows :init (lambda () (eq system-type 'windows-nt)))
+;;    (decl-node :linux :init (lambda () (eq system-type 'gnu/linux)))
+;;    (decl-node :gui :init (lambda () (if window-system t nil)))
+;;    (decl-node :mac-osx-fullscreen-supprt :init
+;;               (lambda ()
+;;                 (defun toggle-fullscreen (&optional f)
+;;                   (interactive)
+;;                   (let ((current-value (frame-parameter nil 'fullscreen)))
+;;                     (set-frame-parameter nil 'fullscreen
+;;                                          (if (equal 'fullboth current-value)
+;;                                              (if (boundp 'old-fullscreen) old-fullscreen nil)
+;;                                            (progn (setq old-fullscreen current-value)
+;;                                                   'fullboth)))))
+;;                 
+;;                 (base-lib-assign-keybindings '("C-s-<268632070>" toggle-fullscreen)) ; command-control-f
+;;                 t)
+;;               '(:gui :mac-osx))
+;;    (decl-node :windows-consolas :init
+;;               (lambda ()
+;;                 (set-face-attribute 'default nil :font "consolas-14:antialias=natural"))
+;;               '(:windows :gui))
+;;    (decl-solve :init)))
+;;
+;; (provide 'init)
+;;
+;; ;;; init.el sample ends here
+
 ;; Variables that the user can modify to alter library behaviour. No tests needed for this code in isolation.
 (defgroup decl ()
   "Customize group for decl.el"
