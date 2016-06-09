a3f97406e3a644fa0ee1733e8330495f282f1c1a
Preetpal Sohal <preetpal.sohal@gmail.com>
2016-06-08 22:50:16 -0700

    Updated decl.el code sample.
    Removed marmalade reference. Removed code that utilized non-publicly available function.

--- decl.el
+++ decl.el
@@ -24,7 +24,6 @@
 ;; ;;; init.el --- sample init.el using library
 ;;
 ;; (package-initialize)
-;; (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 ;; (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 ;;
 ;; (add-hook
@@ -47,7 +46,6 @@
 ;;                                            (progn (setq old-fullscreen current-value)
 ;;                                                   'fullboth)))))
 ;;
-;;                 (base-lib-assign-keybindings '("C-s-<268632070>" toggle-fullscreen)) ; command-control-f
 ;;                 t)
 ;;               '(:gui :mac-osx))
 ;;    (decl-node :windows-consolas :init
